5a350c94c5dbbbd6703e234b77097b2f08d54b8a
chirag <107404264+chimrag@users.noreply.github.com>
2025-12-05 16:25:47 +0530

    Create basicpair.cpp

---
+++ basicpair.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void expalinpair(){
+    pair<int,int>p={1,3};
+    cout<<p.first<<" "<<p.second<<endl;
+    pair<int,int>arr[]={{1,2},{2,3},{3,4}};
+    cout<<arr[1].second;
+}
+
+int main(){
+    expalinpair();
+}
